a36142b1b4606afb5a713cf4e24b93c4c3b591d8
syj <nmrhtn7898@naver.com>
2020-08-09 00:20:45 +0900

    add filter

---
+++ src/main/java/com/example/eurekaclient/filter/UserContextFilter.java
@@ -0,0 +1,42 @@
+package com.example.eurekaclient.filter;
+
+import com.example.eurekaclient.util.UserContext;
+import com.example.eurekaclient.util.UserContextHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * HTTP 요청으로 전달된 헤더 정보들을 UserContext 인스턴스에 셋팅
+ */
+@Slf4j
+@Component
+public class UserContextFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        UserContext context = UserContextHolder.getContext();
+        String correlationId = httpServletRequest.getHeader(UserContext.CORRELATION_ID);
+        context.setCorrelationId(correlationId);
+        context.setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN));
+        context.setOrgId(httpServletRequest.getHeader(UserContext.ORG_ID));
+        context.setUserId(httpServletRequest.getHeader(UserContext.USER_ID));
+        log.debug("route service incoming correlation id : {}", correlationId);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
---
+++ src/main/java/com/example/eurekaclient/util/UserContextHolder.java
@@ -0,0 +1,27 @@
+package com.example.eurekaclient.util;
+
+
+import org.springframework.util.Assert;
+
+/**
+ * 각 쓰레드마다 사용하는 고유한 해시 맵 저장소에 접근하고 관리하는 ThreadLocal 인스턴스에 UserContext 객체를 관리
+ */
+public class UserContextHolder {
+
+    private static final ThreadLocal<UserContext> userContext = new ThreadLocal<>();
+
+    public static UserContext getContext() {
+        UserContext context = userContext.get();
+        if (context == null) {
+            context = new UserContext();
+            userContext.set(context);
+        }
+        return context;
+    }
+
+    public static void setContext(UserContext context) {
+        Assert.notNull(context, "Only non-null UserContext instances are permitted");
+        userContext.set(context);
+    }
+
+}
