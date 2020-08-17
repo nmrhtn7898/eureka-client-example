7b4feca84f7b3218b2e1037d0c44327d9117fabc
syj <nmrhtn7898@naver.com>
2020-08-17 23:10:10 +0900

    update docker settings

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-
 echo "********************************************************"
-echo "waiting for the database server to start on port $DATABASESERVER_PORT"
+echo "waiting for the database2 server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
 while ! `nc -z database2 $DATABASESERVER_PORT`; do sleep 3; done
-echo ">>>>>>>>>>>> Database Server has started"
+echo ">>>>>>>>>>>> Database2 Server has started"
 
 echo "*******************************************"
 echo "run spring cloud eureka client server"
-echo $RUN_MESSAGE
 echo "*******************************************"
 java -jar app.jar
